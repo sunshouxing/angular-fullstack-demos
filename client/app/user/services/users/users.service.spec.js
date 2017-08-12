6ee37773e316a59278c6ced6d8579c703d7235bf
Arthur <shouxing.sun@aliyun.com>
2017-08-12 18:44:10 +0800

    Implement Users service

---
+++ client/app/user/services/users/users.service.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('Service: users', function() {
+  // load the service's module
+  beforeEach(module('meanApp.user.services.users'));
+
+  // instantiate service
+  var users;
+  beforeEach(inject(function(_users_) {
+    users = _users_;
+  }));
+
+  it('should do something', function() {
+    expect(!!users).toBe(true);
+  });
+});
--- client/app/user/user.component.js
+++ client/app/user/user.component.js
@@ -4,15 +4,20 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
 import routes from './user.routes';
+import usersService from './services/users/users.service';
 
 export class UserController {
   /*@ngInject*/
-  constructor() {
+  constructor(Users) {
     this.message = 'Hello';
+    Users.loadAllUsers().then(users => { this.users = users; });
   }
+
+  $onInit() {}
+
 }
 
-export default angular.module('meanApp.user', [uiRouter])
+export default angular.module('meanApp.user', [uiRouter, usersService])
   .config(routes)
   .component('user', {
     template: require('./user.pug'),
