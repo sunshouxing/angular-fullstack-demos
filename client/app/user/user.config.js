ca9426715d51369a39203703e8f03c545db4de6f
Arthur <shouxing.sun@aliyun.com>
2017-08-13 23:39:10 +0800

    Change material theme

--- client/app/user/user.config.js
+++ client/app/user/user.config.js
@@ -8,6 +8,18 @@ export default function($stateProvider, $mdIconProvider, $mdThemingProvider) {
       template: '<user></user>'
     });
 
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue-grey', {
+      'default': '700'
+    })
+    .accentPalette('light-blue', {
+      'default': '600'
+    });
+
+//$mdThemingProvider.theme('default')
+//  .primaryPalette('brown')
+//  .accentPalette('red');
+
   $mdIconProvider.defaultIconSet('assets/svg/avatars.svg', 128)
     .icon('phone', 'assets/svg/phone.svg', 24)
     .icon('twitter', 'assets/svg/twitter.svg', 24)
